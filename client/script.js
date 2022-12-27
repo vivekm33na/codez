ce811cb37a2b7c2c2bc068f7d222786d90f9c2e9
Vivek Meena <vivekm33er@gmai.com>
2022-12-27 23:08:26 +0530

    key updated

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e)=>{
   const response = await fetch('https://codez.onrender.com/',{
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       prompt: data.get('prompt')
